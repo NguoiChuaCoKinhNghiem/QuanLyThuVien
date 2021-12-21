89a02cdb6aa39e09ca6c5b469ae46de4e4bc4853
NguoiChuaCoKinhNghiem <dh51805715@student.stu.edu.vn>
2021-12-21 17:03:07 +0700

    Tao trang quan ly thong tin tac gia

--- demoQuanLyThuVien/demoQuanLyThuVien/frmTacGia.cs
+++ demoQuanLyThuVien/demoQuanLyThuVien/frmTacGia.cs
@@ -20,19 +20,7 @@ namespace demoQuanLyThuVien
         }
         public void hienthi()
         {
-            listView1.View = View.Details;
-            listView1.GridLines = true;
-            listView1.FullRowSelect = true;
-            listView1.Items.Clear();
-            listView1.Columns.Clear();
-            listView1.Columns.Add("Mã tác giả", 100);
-            listView1.Columns.Add("Tên tác giả", 200);
-            foreach(TacGia tg  in db.TacGia.ToList())
-            {
-                ListViewItem lv = listView1.Items.Add(tg.matg);
-                lv.SubItems.Add(tg.tentg);
-
-            }
+           
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
@@ -59,17 +47,7 @@ namespace demoQuanLyThuVien
 
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
-            TacGia tg = db.TacGia.Find(txtTimKiem.Text);
-            if (tg != null)
-            {
-                listView1.Items.Clear();
-                ListViewItem li = listView1.Items.Add(tg.matg);
-                li.SubItems.Add(tg.tentg);
-            }
-            else
-            {
-                MessageBox.Show("Không tìn thấy thông tin tác giả!!!!");
-            }
+            
         }
 
         private void btnHoantat_Click(object sender, EventArgs e)
@@ -79,20 +57,7 @@ namespace demoQuanLyThuVien
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-            TacGia tg = db.TacGia.Find(txtTimKiem.Text);
-            if (tg != null)
-            {
-                MessageBox.Show("Tác giả đã tồn tại, không thể thêm mới!");
-            }
-            else
-            {
-                tg = new TacGia();
-                tg.matg = txtTimKiem.Text;
-                tg.tentg = txtTenTG.Text;
-                db.TacGia.Add(tg);
-                db.SaveChanges();
-                hienthi();
-            }
+           
             
         }
 
